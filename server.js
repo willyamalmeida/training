f0f1ba6b5492e985b7c35582977757dbd14f2d8a
Willyam Santana Almeida <willyamalmeida@hotmail.com>
2018-02-19 22:10:42 -0300

    update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const url = require("url");
 const server = http.createServer((req, res) => {
     debugger;
 
+    setInterval(() => {}, 5000);
+
     if(req.url.indexOf("?") >= 0) {
         let queryParams = url.parse(req.url, true).query;
         res.end("<h1>Query String</h1><br />param: " + JSON.stringify(queryParams));
