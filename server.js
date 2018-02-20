ee35e6c6047bdb0882cd2887d6914715ae39be35
Willyam Santana Almeida <willyam.almeida@lggo.com.br>
2018-02-20 09:25:46 -0300

    update module flow and html

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const url = require("url");
 const server = http.createServer((req, res) => {
     debugger;
 
-    setInterval(() => {}, 5000);
+    setTimeout(() => {
 
-    if(req.url.indexOf("?") >= 0) {
-        let queryParams = url.parse(req.url, true).query;
-        res.end("<h1>Query String</h1><br />param: " + JSON.stringify(queryParams));
-    }
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+    
+        if(req.url.indexOf("?") >= 0) {
+            let queryParams = url.parse(req.url, true).query;
+            res.end("<h1>Query String</h1><br />param: " + JSON.stringify(queryParams));
+        }
+    
+        if(req.url === "/" || req.url === "/home"){
+            res.end("<h1>Home</h1>");
+        }
+    
+        res.end("<h1>Not found</h1>");
 
-    if(req.url === "/"){
-        res.end("<h1>Home</h1>");
-    }
+    }, 5000);
 
-    res.end("<h1>Not found</h1>");
 });
 
 server.listen(8080, "localhost", () => {
