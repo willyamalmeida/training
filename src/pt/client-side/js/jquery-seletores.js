c067f12657eb057a297546f46ed0e0090b67e7c8
Willyam Santana Almeida <willyam.almeida@lggo.com.br>
2018-02-27 08:34:24 -0300

    demo jquery

---
+++ src/pt/client-side/js/jquery-seletores.js
@@ -0,0 +1,39 @@
+$("body")
+    .css("font-size", "14px")
+    .css("font-family", "helvetica");
+
+$(".separadorDeComponente")
+    .css("border", "1px dashed #ddd")
+    .css("padding", "5px");
+
+$("body > .titulo")
+    .css("font-size","36px")
+    .css("margin","10px 0px");
+
+/* Componente 1 */
+/* ************ */
+
+$(".componente1")
+    .css("margin-bottom", "10px");
+
+// .componente1 .titulo { 
+  
+// }
+
+// .componente1 .listaDeItens { 
+
+// }
+
+$(".componente1 .listaDeItens .item")
+    .css("border", "1px solid #ddd")
+    .css("margin", "10px 0px")
+    .css("padding", "5px");
+
+$(".componente1 .listaDeItens .item .titulo")
+    .css("color", "navy");
+
+/* Componente 2 */
+/* ************ */
+
+$(".componente2 .titulo")
+    .css("color","red");
